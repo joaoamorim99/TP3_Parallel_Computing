e51eadc7398ab34ee2a17b24c571c5b343284d49
Mariana Marques <marianamarquesworkplace@gmail.com>
2022-12-21 00:38:35 +0000

    some things

--- k_means.c
+++ k_means.c
@@ -31,6 +31,7 @@ void initNodo(Nodo n, int n_samples, int n_clusters) {
     for(int i = 0; i < n_clusters; i++) { 
         n->centroids_x[i] = n->samples_x[i]; //<<cluster_i_coordenada_x>> = <<ponto_i_coordenada_x>> 
         n->centroids_y[i] = n->samples_y[i]; //<<cluster_i_coordenada_y>> = <<ponto_i_coordenada_y>>  
+        //MPI_SEND or BROADCAST
     } 
 }
 
@@ -42,7 +43,18 @@ void initNodo(Nodo n, int n_samples, int n_clusters) {
         #pragma omp parallel for reduction (+:temp_ind[:size_c], temp_c_x[:size_c], temp_c_y[:size_c]) num_threads (n_threads) 
         for(int i=0; i < size_s; i++) {
     
-            
+            int cluster = 0;
+            float min = 1, dist;
+
+            for(int j=0; j < size_c; j++) { 
+
+                dist = euc_dist(samples_x[i], samples_y[i], centroids_x[j], centroids_y[j]);
+                
+                if(dist<min) {
+                    min = dist;
+                    cluster = j;
+                }
+            }
 
             temp_c_x[cluster] += samples_x[i];
             temp_c_y[cluster] += samples_y[i];
